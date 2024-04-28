d38a1ea3a1359c826c03f3704427589ef06350f7
aniya-mitchell <156115896+aniya-mitchell@users.noreply.github.com>
2024-04-28 13:41:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['/assets/index-BIqKQMxO.js']
+      external: ['react']
     }
   }
-}
+})
+
